f3a82cb746bfa7a8fce91fb87962eafba14a24eb
Özkan <karaaslanozkan@hotmail.com>
2022-09-20 17:52:40 +0300

    qwe

--- src/pages/QRscanner.js
+++ src/pages/QRscanner.js
@@ -1,16 +1,28 @@
-import React, {useState} from 'react'
+import React, {useState,useEffect} from 'react'
 import {Fab, TextareaAutosize} from '@material-ui/core'
 import {ArrowBack} from '@material-ui/icons'
 import { Link } from "react-router-dom";
 import QrScan from 'react-qr-reader'
 import axios from 'axios';
 function QRscanner() {
-
+    useEffect(()=>{
+        axios("https://jsonplaceholder.typicode.com/users/1")
+        .then((res) => setProduct(res.data))
+        .catch((e) => console.log(e))
+        .finally(() => setIsLoading(false));
+      });
+      const [mounturl, setMounturl] = useState("");
+      const [isLoading, setIsLoading] = useState(true);
+      const [product, setProduct] = useState([]);   
     const [qrscan, setQrscan] = useState('No result');
     const handleScan = data => {
         
         if (data) {
-            setQrscan(data)
+            var productId=data.split("/");
+            axios( "https://jsonplaceholder.typicode.com/users/" +productId[3])
+            .then((res) => setProduct(res.data))
+            .catch((e) => console.log(e))
+            .finally(() => setIsLoading(false));
         }
     }
     const handleError = err => {
@@ -36,8 +48,8 @@ function QRscanner() {
             <TextareaAutosize
                 style={{fontSize:18, width:320, height:100, marginTop:100}}
                 rowsMax={4}
-                defaultValue={qrscan}
-                value={qrscan}
+                defaultValue={product.name}
+                value={product.name}
             />
 
       </div>
